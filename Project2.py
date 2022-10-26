cf2e0f0fd308936fddb69b14c8539b79a57cb182
krhamby <hambykr19@gcc.edu>
2022-10-25 22:26:38 -0400

    Added better error messages

--- Project2.py
+++ Project2.py
@@ -3,6 +3,8 @@ import base64 as encoder
 import os
 import secrets
 
+from colorama import Fore
+
 # Use the following technique to store password
 # Base64 ( SHA-512 ( password || Base64( salt )))
 
@@ -23,18 +25,23 @@ if __name__ == '__main__':
             username = command[1]
             password = command[2]
             
-            # generate salt and encode with Base64
-            ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            salt = ''.join(secrets.choice(ALPHABET) for i in range(16))
-            salt = encoder.b64encode(salt.encode('utf-8'))
+            # make sure username and password are in ascii
+            if not username.isascii or not password.isascii:
+                print("\u001b[31mUsername and password must use ASCII characters\u001b[0m\n")
+                continue
+            else:            
+                # generate salt and encode with Base64
+                ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                salt = ''.join(secrets.choice(ALPHABET) for i in range(16))
+                salt = encoder.b64encode(salt.encode('ascii'))
+                
+                # hash and encode password + salt
+                passHash = encoder.b64encode(hash.sha512((password + salt.decode('ascii')).encode()).digest())
+                
+                # store in file
+                with open("Project2PW.txt", 'a') as file:
+                    file.write(username + ":$6$" + salt.decode('ascii') + "$" + passHash.decode('ascii') + "\n")
             
-            # hash and encode password + salt
-            passHash = encoder.b64encode(hash.sha512((password + salt.decode('utf-8')).encode()).digest())
-            
-            # store in file
-            with open("Project2PW.txt", 'a') as file:
-                file.write(username + ":$6$" + salt.decode('utf-8') + "$" + passHash.decode('utf-8') + "\n")
-        
         # check-password <username> <password>
         elif len(command) == 3 and command[0] == "check-password":
             username = command[1]
@@ -56,13 +63,13 @@ if __name__ == '__main__':
             passHash = passHash.strip()
             
             # encode passHash from string to bytes
-            passHash.encode('utf-8')
+            passHash.encode('ascii')
             
             # compare with stored hash
-            if passHash == encoder.b64encode(hash.sha512((password + salt).encode()).digest()).decode('utf-8'):
-                print("Password is correct\n")
+            if passHash == encoder.b64encode(hash.sha512((password + salt).encode()).digest()).decode('ascii'):
+                print("\u001b[32mPassword is correct\u001b[0m \n")
             else:
-                print("Password is incorrect\n")
+                print("\u001b[31mPassword is incorrect\u001b[0m \n")
         
         # remove-user <username>
         elif len(command) ==2 and command[0] == "remove-user":
@@ -89,7 +96,7 @@ if __name__ == '__main__':
             break
         
         else:
-            print("Invalid command\n")
+            print("\u001b[31mInvalid command\u001b[0m\n")
             continue
         
         
